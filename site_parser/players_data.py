9e90e0093fc444dced0907c28643571ffbc6db1e
lewyino <mateusz.lewandowski@gmail.com>
2023-08-28 19:11:21 +0200

    new TeamPlayer class, add test to check parse players data (compare it)

--- site_parser/players_data.py
+++ site_parser/players_data.py
@@ -2,26 +2,26 @@ from bs4 import BeautifulSoup
 from typing import List
 import re
 
-from model.Player import Player
+from model.TeamPlayer import TeamPlayer
 
 
-def get_players_list(page: str) -> List[Player]:
+def get_players_list(page: str) -> List[TeamPlayer]:
     soup = BeautifulSoup(page, 'html5lib')
     players = []
     for line_name in ('offense', 'defense', 'special'):
         line_data = soup.find('div', attrs={'id': line_name})
         for player_row_raw in line_data.find_all('div', recursive=False):
             player_data = player_row_raw.select('div.row_cell')
-            player_country = player_data.pop(0).find('img').get('alt')
-            player_raw_data = player_data.pop(0)
+            # player_country = player_data.pop(0).find('img').get('alt')
+            player_raw_data = player_data.pop(1)
             player_id = re.search(r'.*/playerid/(\d+)', player_raw_data.find('a').get('href')).groups()[0]
             player_name = player_raw_data.find('a').text
-            player_bpos = player_data.pop(0).text
-            player_age = player_data.pop(0).text
-            player_rating = re.search(r'.*rating_(\d+).*', player_data.pop(0).find('img').get('src')).groups()[0]
-            player_energy = player_data.pop(0).text.replace('%', '')
-            player_wage = player_data.pop(0).text.replace('$', '').strip()
-            player_health = player_data.pop(0).text
-            p = Player(player_id, player_name, player_bpos, player_age, player_rating, player_energy, player_wage, player_health)
+            # player_bpos = player_data.pop(0).text
+            # player_age = player_data.pop(0).text
+            # player_rating = re.search(r'.*rating_(\d+).*', player_data.pop(0).find('img').get('src')).groups()[0]
+            # player_energy = player_data.pop(0).text.replace('%', '')
+            # player_wage = player_data.pop(0).text.replace('$', '').strip()
+            # player_health = player_data.pop(0).text
+            p = TeamPlayer(player_id, player_name)
             players.append(p)
     return players
--- test/players_data_test.py
+++ test/players_data_test.py
@@ -1,3 +1,4 @@
+from model.TeamPlayer import TeamPlayer
 from site_parser.players_data import get_players_list
 import os
 
@@ -7,4 +8,14 @@ def test_get_players_list_count():
     f = open(f'{os.path.dirname(__file__)}/players.html', 'r')
     players_site_data = f.read()
     f.close()
-    assert len(get_players_list(players_site_data)) == __result
+    result = get_players_list(players_site_data)
+    assert len(result) == __result
+
+
+def test_get_players_list_data():
+    __result = TeamPlayer(123456, 'Jan Kowalski')
+    f = open(f'{os.path.dirname(__file__)}/players.html', 'r')
+    players_site_data = f.read()
+    f.close()
+    result = get_players_list(players_site_data)
+    assert result[0].__dict__ == __result.__dict__
