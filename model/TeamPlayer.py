108bbc9110641741f7569191dcffeac5043ff57b
lewyino <mateusz.lewandowski@gmail.com>
2023-09-02 10:50:59 +0200

    parse player data, basic + skills

--- model/TeamPlayer.py
+++ model/TeamPlayer.py
@@ -5,29 +5,13 @@ class TeamPlayer:
         self.id = int(_id) if type(_id) == str else _id
         self.name = " ".join(name.split())
 
-    def __setattr__(self, *args, **kwargs):
-        return super().__setattr__(*args, **kwargs)
-
-    def __getattribute__(self, *args, **kwargs):
-        try:
-            if args[0] in ('rating', 'experience', 'talent', 'teamChemistry'):
-                return super().__getattribute__(*args, **kwargs) / 2
-            return super().__getattribute__(*args, **kwargs)
-        except:
-            if args[0] in ('name', 'bpos', 'wage', 'health'):
-                return ''
-            else:
-                return 0
-
     def __str__(self):
         return f'id: {self.id}\n' \
                f'name: {self.name}\n' \
-               f'bpos: {self.bpos}\n' \
-               f'age: {self.age}\n' \
-               f'rating: {self.rating}*\n' \
-               f'energy: {self.energy}%\n' \
-               f'wage: {self.wage}$\n' \
-               f'health: {self.health}\n'
+               f'age: {getattr(self, "age")}\n' \
+               f'rating: {getattr(self, "rating")}*\n' \
+               f'energy: {getattr(self, "energy")}%\n' \
+               f'wage: {getattr(self, "wage")}$\n' \
 
     def __repr__(self):
         return self.__str__()
--- site_parser/players_data.py
+++ site_parser/players_data.py
@@ -1,5 +1,5 @@
-from bs4 import BeautifulSoup
-from typing import List
+from bs4 import BeautifulSoup, Tag
+from typing import List, Dict
 import re
 
 from model.TeamPlayer import TeamPlayer
@@ -12,16 +12,78 @@ def get_players_list(page: str) -> List[TeamPlayer]:
         line_data = soup.find('div', attrs={'id': line_name})
         for player_row_raw in line_data.find_all('div', recursive=False):
             player_data = player_row_raw.select('div.row_cell')
-            # player_country = player_data.pop(0).find('img').get('alt')
             player_raw_data = player_data.pop(1)
             player_id = re.search(r'.*/playerid/(\d+)', player_raw_data.find('a').get('href')).groups()[0]
             player_name = player_raw_data.find('a').text
-            # player_bpos = player_data.pop(0).text
-            # player_age = player_data.pop(0).text
-            # player_rating = re.search(r'.*rating_(\d+).*', player_data.pop(0).find('img').get('src')).groups()[0]
-            # player_energy = player_data.pop(0).text.replace('%', '')
-            # player_wage = player_data.pop(0).text.replace('$', '').strip()
-            # player_health = player_data.pop(0).text
             p = TeamPlayer(player_id, player_name)
             players.append(p)
     return players
+
+
+def __get_player_data_basic_info_top(basic_info_top: Tag) -> Dict[str, str]:
+    tags = basic_info_top.select('div.player_basic_info_row')
+    player_basic_info_groups = re.search(r'(.*)\s*\(ID\s*(\d+)', tags.pop(0).select('td').pop(1).text).groups()
+    player_name = player_basic_info_groups[0].strip()
+    player_id = player_basic_info_groups[1].strip()
+    player_age = re.search(r'(\d+).*', tags.pop(1).select('td').pop(0).text).groups()[0]
+    return {
+        'player_name': player_name,
+        'player_id': player_id,
+        'player_age': player_age
+    }
+
+
+def __get_player_data_basic_info_bottom(basic_info_bottom: Tag) -> Dict[str, float | int]:
+    player_ratings_info = basic_info_bottom.select('td')
+    player_rating = re.search(r'.*rating_(\d+).*', player_ratings_info[1].find('img').get('src')).groups()[0]
+    player_experience = re.search(r'.*rating_(\d+).*', player_ratings_info[3].find('img').get('src')).groups()[0]
+    player_energy = player_ratings_info[5].text.replace('%', '').strip()
+    player_talent = re.search(r'.*rating_(\d+).*', player_ratings_info[7].find('img').get('src')).groups()[0]
+    player_team_chemistry = re.search(r'.*rating_(\d+).*', player_ratings_info[9].find('img').get('src')).groups()[0]
+    player_wage_groups = re.search(r'.*(\d+),(\d+).*', player_ratings_info[11].text).groups()
+    return {
+        'rating': round(float(player_rating) / 2, 1),
+        'experience': round(float(player_experience) / 2, 1),
+        'energy': int(player_energy),
+        'talent': round(float(player_talent) / 2, 1),
+        'team_chemistry': round(float(player_team_chemistry) / 2, 1),
+        'wage': int(player_wage_groups[0] + player_wage_groups[1])
+    }
+
+
+def __get_player_data_skills(player_profile: Tag) -> Dict[str, int]:
+    tags = player_profile.select('div.player_data_wrapper span')
+    skills = {}
+    for tag in tags:
+        skill_name = tag.find_parent().find_previous_sibling().text.strip().lower().replace(' ', '_')
+        skill_value = tag.text.strip()
+        skills[skill_name] = int(skill_value)
+    return skills
+
+
+def __get_player_data_body(player_profile: Tag) -> Dict[str, float]:
+    tags = player_profile.select('div.player_data_wrapper b')
+    body = {}
+    for i in range(3):
+        body_name = tags[i].find_parent().find_previous_sibling().text.strip().lower().split(' ')[0]
+        body_value = tags[i].text.strip()
+        body[body_name] = float(body_value)
+    return body
+
+
+def get_player_data(page: str) -> TeamPlayer:
+    soup = BeautifulSoup(page, 'html5lib')
+    basic_info_top = soup.find('div', attrs={'class': 'player_basic_info_top'})
+    player_data = __get_player_data_basic_info_top(basic_info_top)
+    player = TeamPlayer(player_data['player_id'], player_data['player_name'])
+    player.age = int(player_data['player_age'])
+    basic_info_bottom = soup.find('div', attrs={'class': 'player_basic_info_bottom'})
+    for basic_data_name, basic_data_value in __get_player_data_basic_info_bottom(basic_info_bottom).items():
+        setattr(player, basic_data_name, basic_data_value)
+    player_profile = soup.find('div', attrs={'id': 'profile'})
+    for skill_name, skill_value in __get_player_data_skills(player_profile).items():
+        setattr(player, skill_name, skill_value)
+    for body_name, body_value in __get_player_data_body(player_profile).items():
+        setattr(player, body_name, body_value)
+    return player
+
